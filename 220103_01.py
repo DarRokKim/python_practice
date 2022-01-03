23484c61ba1141a5ea9032988b73a9a2267d70a3
DaeRokKim <kdr0112@gmail.com>
2022-01-03 16:02:06 +0900

    python practice 220103

---
+++ 220103_01.py
@@ -0,0 +1,107 @@
+# def print_if_positive(number):
+#     if number >=  0: 
+#         print(number)
+
+# print_if_positive(1)
+# print_if_positive(-1)
+
+# #####################
+
+# def print_whether_positive_or_negetive(number):
+#     if number >= 0:
+#         print('+')
+#     else:
+#         print('-')
+
+# print_whether_positive_or_negetive(1)
+# print_whether_positive_or_negetive(-1)
+
+# ########################
+
+# def print_whether_positive_or_negative_or_zero(number):
+#     if number > 0:
+#         print('+')
+#     elif number == 0:
+#         print('0')
+#     else:
+#         print('-')
+
+# print_whether_positive_or_negative_or_zero(1)
+# print_whether_positive_or_negative_or_zero(0)
+# print_whether_positive_or_negative_or_zero(-1)
+
+# ####################
+
+# def print_if_positive_and_even(number):
+#     if (number > 0) and (number % 2 == 0):
+#         print(number)
+
+# print_if_positive_and_even(1)
+# print_if_positive_and_even(-1)
+# print_if_positive_and_even(2)
+# print_if_positive_and_even(-2)
+
+# #################
+
+# def print_if_negative_or_odd(number):
+#     if (number < 0) or (number % 2 != 0):
+#         print(number)
+
+# print_if_negative_or_odd(1)
+# print_if_negative_or_odd(-1)
+# print_if_negative_or_odd(2)
+# print_if_negative_or_odd(-2)
+
+# ##############
+
+# def print_if_negative_or_odd(number):
+#     if (number <= 0) or (number % 2 != 0):
+#         print(number)
+
+# print_if_negative_or_odd(1)
+# print_if_negative_or_odd(-1)
+# print_if_negative_or_odd(2)
+# print_if_negative_or_odd(-2)
+
+# #############
+
+# # while 반복문
+
+# def fibonacci(n):
+#     if n <= 2:
+#         return 1
+#     else:
+#         return fibonacci(n-2) + fibonacci(n-1)
+
+# n = 1
+# while n <= 20:
+#     print(fibonacci(n))
+#     n = n + 1
+# print("끝!")
+
+# #############
+
+# # for 반복문
+
+# for character in 'Hello':
+#     print(character)
+
+# #############
+
+# print('Quote(\') and double quote(\")')
+
+# ##############
+
+# list_123 = [1,2,3]
+# list_123.append(4)
+# print(list_123)
+
+# list_123.remove(4)
+# print(list_123)
+
+# list_123.pop(0)
+# print(list_123)
+
+# ##############
+
+
